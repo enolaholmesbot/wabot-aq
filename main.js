4fdc78e9d142f769e3bfb85f71a1a9f04e2e28ac
117188 <77671181+117188@users.noreply.github.com>
2021-02-19 23:38:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6281515860089'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['60195042951'] // Put your number here
+global.mods = [60195042951] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
