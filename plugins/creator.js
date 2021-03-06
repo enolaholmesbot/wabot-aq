89876c1315f74a6485d707c8584c7af8eeeebbfe
117188 <77671181+117188@users.noreply.github.com>
2021-03-06 16:24:29 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '60195042951', 'Nurutomo', m)
-  this.sendContact(m.chat, '60195042951', 'ahmad beban ', m)
+  // this.sendContact(m.chat, '601114231152', 'Nurutomo', m)
+  this.sendContact(m.chat, '601114231152', 'gpp sumpah gpp walau pun nomor gw di banned gw tetep berkayu kok🥰', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
