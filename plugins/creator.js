6352917f6f94c338c731478e691cdf567251defa
117188 <77671181+117188@users.noreply.github.com>
2021-03-07 08:26:39 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '601114230813', 'Nurutomo', 
-  this.sendContact(m.chat, '601114230813', 'Ahmad', m)
+  this.sendContact(m.chat, '601114230813', 'owner gak on', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
